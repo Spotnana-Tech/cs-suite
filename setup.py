b25a8da8480dfe048c6242ab8f3dd7944c258145
Sameer Pande <sameer@spotnana.com>
2020-07-27 14:59:22 +0530

    adding compressed ScoutSuite tar files instead of entire directory

--- setup.py
+++ setup.py
@@ -5,16 +5,19 @@ import subprocess
 def main():
     """ main function """
     try:
-        print ("Installing dependencies...")
-        subprocess.call(['sudo', 'pip', 'install', '-r', 'requirements.txt'])
-        print ("Installing AWS CLI") # WINDOWS HAS NO SUDO
-        subprocess.call(['sudo', 'pip', 'install', 'awscli', '--ignore-installed', 'six'])
-        print ("Installing sshpass")
-        subprocess.call(['sudo', 'tar', '-xvf', 'sshpass-1.06.tar.gz'], cwd = './tools')
-        subprocess.check_output(['sudo', './configure'], cwd='tools/sshpass-1.06')
-        subprocess.call(['sudo', 'make', 'install'], cwd='tools/sshpass-1.06/')
-        print ("Please enter your AWS credetionals")
-        subprocess.call(['aws', 'configure'])
+        # print ("Installing dependencies...")
+        # subprocess.call(['sudo', 'pip', 'install', '-r', 'requirements.txt'])
+        # print ("Installing AWS CLI") # WINDOWS HAS NO SUDO
+        # subprocess.call(['sudo', 'pip', 'install', 'awscli', '--ignore-installed', 'six'])
+        # print ("Installing sshpass")
+        # subprocess.call(['sudo', 'tar', '-xvf', 'sshpass-1.06.tar.gz'], cwd = './tools')
+        # subprocess.check_output(['sudo', './configure'], cwd='tools/sshpass-1.06')
+        # subprocess.call(['sudo', 'make', 'install'], cwd='tools/sshpass-1.06/')
+        # print ("Please enter your AWS credetionals")
+        # subprocess.call(['aws', 'configure'])
+        
+        print ("Installing Scout Suite")
+        subprocess.call(['sudo', 'tar', '-xvf', 'ScoutSuite.tar.xz'], cwd = './tools')
         print("Setting up Scout Suite")
         subprocess.call(['../../scripts/setup_scout_suite.sh'],cwd='tools/ScoutSuite/')
     except:
